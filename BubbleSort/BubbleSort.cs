371ee98304d62f8ccc7ca3786f199d5ed7dede66
c0de4un <code4un@yandex.ru>
2024-03-26 01:10:10 +0400

    Implement BubbleSort

--- BubbleSort/BubbleSort.cs
+++ BubbleSort/BubbleSort.cs
@@ -5,12 +5,35 @@
 /// </summary>
 public static class BubbleSort
 {
+    private static void Swap(ref int a, ref int b)
+    {
+        var temp = a;
+        e1 = b;
+        b = temp;
+    }
+
     /// <summary>
     /// Сортирвать элементы массива.
     /// </summary>
     /// <param name="array">Массив для сортировки.</param>
     public static void Sort(int[] array)
     {
-        // Здесь нужно реализовать сортировку пузырьком.
+        var len = array.Length;
+        for (var i = 1; i < len; i++)
+        {
+            for (var j = 0; j < len - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                }
+            }
+        }
+
+        return array;
     }
+
+// 3. Для пузырьковой сортировки выполнить оптимзиации
+//   - не учитывать в сравнении пар последний элемент после каждого прохода по массиву, так как он уже отсортирован;
+//   -  добавить проверку того, что массив отсортирован, чтобы избежать бесполезных проходов по массиву, если он уже отсортирован.
 }
